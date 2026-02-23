1e31764c7fcc4a2e9f110be60138c3a156cfd539
Rishabh <rishabhdasu@gmail.com>
2026-02-23 10:42:18 +0530

    feat(frontend): implement axios request interceptor for automated token handling

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,11 +2,24 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1",
-  timeout: 6000,
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
 });
+
+axiosInstance.interceptors.request.use(
+  (config)=> {
+    const token = localStorage.getItem("token");
+    if(token) {
+      config.headers.Authorization = `Bearer${token}`
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+)
 
 export default axiosInstance;
